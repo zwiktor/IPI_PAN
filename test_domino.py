9d754d0834402be416b0df9e4c886436cb477a59
zwiktor <zwiktor2023032@gmail.com>
2021-03-22 13:55:49 +0100

    prepare basics test for exercises

--- test_domino.py
+++ test_domino.py
@@ -6,18 +6,22 @@ class TestDominoInstance(unittest.TestCase):
 
 
     def setUp(self):
-        self.dominos = Domino(r'//|||||\\\\|')
+        self.dominos = Domino(r'||//||\||/\|')
+
 
     def test_domino_atribute(self):
-        self.assertEqual(self.dominos.dominos, '//|||||\\\\|')
+        self.assertTrue(hasattr(self.dominos, 'dominos'))
+        self.assertEqual(self.dominos.dominos, r'||//||\||/\|')
         for dom in self.dominos.dominos:
-            self.assertIn(dom, ['right', 'left', 'center'])
+            self.assertIn(dom, ['\\', '|', '/'])
 
     def test_domino_next_iteral(self):
-        self.assertEqual(self.dominos.next_iteral(self.dominos), r'///|||\\\\\|')
+        self.dominos.next_iteral()
+        self.assertEqual(self.dominos.dominos, r'||///\\||/\|')
 
     def test_domino_previous_iteral(self):
-        self.assertEqual(self.dominos.previous_iteral(), r'/|||||||\\\|')
+        self.dominos.previous_iteral()
+        self.assertEqual(self.dominos.dominos, r'||/|||\||/\|')
 
 
 if __name__ == "__main__":
